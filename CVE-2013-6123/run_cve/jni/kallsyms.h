136ce85d37697d460aa96b8d240355df9e5ba5fc
Tesi1a <tangsilian@users.noreply.github.com>
2019-01-12 17:55:08 +0800

    add cve-2013-6123
    cve-2013-6123分析

---
+++ CVE-2013-6123/run_cve/jni/kallsyms.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2013 Hiroyuki Ikezoe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef KALLSYMS_H
+#define KALLSYMS_H
+
+#include <stdbool.h>
+
+bool  kallsyms_exist(void);
+void *kallsyms_get_symbol_address(const char *symbol_name);
+
+#endif /* KALLSYMS_H */
+/*
+vi:ts=2:nowrap:ai:expandtab:sw=2
+*/
